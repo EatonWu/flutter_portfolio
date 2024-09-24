2e84e33352517d4b5126edeebd9dc63472e3b245
Eaton <eatonwu100@hotmail.com>
2024-09-23 21:20:46 -0700

    Got color splashes generating within the stack

---
+++ lib/RadialGradientColorSplash.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+List<MaterialColor> generateRandomListOfMaterialColors(int quantity) {
+  return List<MaterialColor>.generate(
+    quantity,
+    (index) => Colors.primaries[index % Colors.primaries.length],
+  );
+}
+
+List<Color> generateRandomListOfColors(int quantity) {
+  return List<Color>.generate(
+    quantity,
+    (index) => Colors.primaries[index % Colors.primaries.length],
+  );
+}
+
+RadialGradientColorSplash radialGradientColorSplash(Color startColor, Color endColor) {
+  return RadialGradientColorSplash(
+    startColor: startColor,
+    endColor: endColor,
+  );
+}
+
+class RadialGradientColorSplash extends StatelessWidget {
+  final Color startColor;
+  final Color endColor;
+  final double minSize;
+  final double maxSize;
+
+  const RadialGradientColorSplash({
+    super.key,
+    required this.startColor,
+    required this.endColor,
+    this.minSize = 50.0,
+    this.maxSize = 150.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Generate random size between minSize and maxSize
+    var randomSize = Random().nextDouble() * (maxSize - minSize) + minSize;
+
+    var middleColor = Color.lerp(startColor, endColor, 0.5) ?? Colors.white;
+
+    return Container(
+      width: randomSize,
+      height: randomSize,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: RadialGradient(
+          colors: [
+            startColor,
+            middleColor,
+            endColor,
+          ],
+          stops: const [0.3, 0.7, 1.0],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:portfolio_site/RadialGradientColorSplash.dart';
+import 'dart:async';
 
 final _router = GoRouter(
-  routes: [
+  initialLocation: '/home',
+  errorBuilder: (context, state) {
+    return const ErrorPage();
+  },
+  routes: <RouteBase>[
     GoRoute(
-      path: '/',
-      builder: (context, state) => HomeScreen(),
+      name: 'home',
+      path: '/home',
+      builder: (context, state) => const MyHomePage(title: 'Flutter Demo Home Page'),
     ),
+    GoRoute(
+      name: '404',
+      path: '/404',
+      pageBuilder: (context, state) => MaterialPage(
+        key: state.pageKey,
+        child: Scaffold(
+          appBar: AppBar(title: const Text('404')),
+          body: const Center(child: Text('404')),
+        ),
+      ),
+    )
   ],
 );
 
+
+
 void main() {
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
@@ -20,28 +44,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    // set primary color to a dark color
+
+    return MaterialApp.router(
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: const ColorScheme.dark().copyWith(
+          primary: Colors.blueGrey,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
     );
   }
 }
@@ -65,71 +76,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Widget> splashes = [];
+  Timer? _timer;
+  int _circleCount = 0;
+  final int _maxCircles = 100;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    // Start a timer that adds a new splash every 0.5 seconds
+    _timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+      if (_circleCount < _maxCircles) {
+        setState(() {
+          var randomListOfColors = generateRandomListOfColors(20);
+          var randomColor1 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
+          var randomColor2 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
+          splashes.add(
+              radialGradientColorSplash(
+                  randomColor1,
+                  randomColor2
+              )
+          );
+          _circleCount++;
+        });
+      } else {
+        _timer?.cancel(); // Stop the timer once the limit is reached
+      }
     });
   }
 
   @override
+  void dispose() {
+    _timer?.cancel(); // Ensure timer is canceled when the widget is disposed
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Column(
+        children: [
+          Flexible(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('signature.png'),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 10,
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Stack(
+                    children: [
+                      ...splashes,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page not found'),
+      ),
+      body: const Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            // massive shrug emoji
+            Text('🤷‍♂️', style: TextStyle(fontSize: 96)),
+            Text('Page not found', style: TextStyle(fontSize: 24)),
+            Text('404'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
