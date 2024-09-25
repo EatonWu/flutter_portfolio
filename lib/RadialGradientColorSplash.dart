4aa74c78fcbd3ced7e96f4bba64ac42d55a7e280
Eaton <eatonwu100@hotmail.com>
2024-09-24 18:37:22 -0700

    Completed UI, need to add linking and cgg demo

--- lib/RadialGradientColorSplash.dart
+++ lib/RadialGradientColorSplash.dart
@@ -43,11 +43,11 @@ class RadialGradientColorSplash extends StatelessWidget {
     // Generate random size between minSize and maxSize
     var randomSize = Random().nextDouble() * (maxSize - minSize) + minSize;
     var middleColor = Color.lerp(startColor, endColor, 0.5) ?? Colors.white;
-
+    var radius = randomSize / 2;
     return LayoutBuilder(
       builder: (context, constraints) {
-        var randomPositionTop = Random().nextDouble() * constraints.maxHeight;
-        var randomPositionLeft = Random().nextDouble() * constraints.maxWidth;
+        var randomPositionTop = (Random().nextDouble() * constraints.maxHeight) - radius;
+        var randomPositionLeft = (Random().nextDouble() * constraints.maxWidth) - radius;
         return Stack(
           children: [
             Positioned(
--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,25 @@ final _router = GoRouter(
     GoRoute(
       name: 'home',
       path: '/home',
-      builder: (context, state) => const MyHomePage(title: 'Flutter Demo Home Page'),
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const MyHomePage(title: 'Portfolio Home'),
+      ),
     ),
     GoRoute(
-      name: '404',
-      path: '/404',
-      pageBuilder: (context, state) => MaterialPage(
+      name: 'about',
+      path: '/about',
+      pageBuilder: (context, state) => NoTransitionPage<void>(
         key: state.pageKey,
-        child: Scaffold(
-          appBar: AppBar(title: const Text('404')),
-          body: const Center(child: Text('404')),
-        ),
+        child: const AboutPage(title: 'About Page')
+      ),
+    ),
+    GoRoute(
+      name: 'demos',
+      path: '/demos',
+      pageBuilder: (context, state) => NoTransitionPage<void>(
+        key: state.pageKey,
+        child: const DemoPage(title: 'Demos')
       ),
     )
   ],
@@ -51,6 +59,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: const ColorScheme.dark().copyWith(
           primary: Colors.blueGrey,
+          surface: Colors.black87,
         ),
       ),
       routerConfig: _router,
@@ -84,12 +93,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    // random value between 250 and 1000
+    var randomDuration = Random().nextInt(750) + 250;
     super.initState();
-    // Start a timer that adds a new splash every 0.5 seconds
-    _timer = Timer.periodic(const Duration(milliseconds: 200), (timer) {
+    _timer = Timer.periodic(Duration(milliseconds: randomDuration), (timer) {
       if (_circleCount < _maxCircles) {
         setState(() {
-          var randomListOfColors = generateRandomListOfColors(20);
+          var randomListOfColors = generateRandomListOfColors(30);
           var randomColor1 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
           var randomColor2 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
           splashes.add(
@@ -98,12 +108,26 @@ class _MyHomePageState extends State<MyHomePage> {
                   randomColor2
               )
           );
+          randomDuration = Random().nextInt(750) + 250;
           _circleCount++;
         });
       } else {
-        _timer?.cancel(); // Stop the timer once the limit is reached
+        _timer?.cancel();
       }
     });
+
+    for (var i = 0; i < 50; i++) {
+      var randomListOfColors = generateRandomListOfColors(30);
+      var randomColor1 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
+      var randomColor2 = randomListOfColors[Random().nextInt(randomListOfColors.length)];
+      splashes.add(
+          radialGradientColorSplash(
+              randomColor1,
+              randomColor2
+          )
+      );
+      _circleCount++;
+    }
   }
 
   @override
@@ -114,37 +138,188 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-        title: Text(widget.title),
+    return Title(
+      title: widget.title,
+      color: Theme.of(context).colorScheme.primary,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Stack(
+                      // fit: StackFit.expand,
+                      children: [
+                        ...splashes,
+                      ],
+                    ),
+                  ),
+                  const Expanded(
+                    flex: 5,
+                      child: HomePageMainContent(),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Stack(
+                      // fit: StackFit.expand,
+                      children: [
+                        ...splashes,
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Row(
+    );
+  }
+}
+
+class HomePageMainContent extends StatelessWidget {
+  const HomePageMainContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('signature.png'),
+          ],
+        ),
+        NavTabBar(),
+        Expanded(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset('signature.png'),
+              // circle buttons with github, linkedin, and email
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Flexible(
+                    child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text("Welcome!",
+                          style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 96,
+                          )
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // open github
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(Icons.code),
+                        Text(' GitHub'),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // open linkedin
+                      },
+                      child: const Row(
+                        children: [
+                          Icon(Icons.person),
+                          Text(' LinkedIn'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // open email
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(Icons.email),
+                        Text(' Email'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
             ],
           ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      ...splashes,
-                    ],
-                  ),
-                ),
-              ],
+        )
+      ],
+    );
+  }
+}
+
+class NavTabBar extends StatefulWidget {
+  int selectedIndex = 0;
+  int size = 3;
+  NavTabBar({super.key});
+  @override
+  State<NavTabBar> createState() => _NavTabBarState();
+}
+
+class _NavTabBarState extends State<NavTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    var currentLoc = GoRouterState.of(context).name;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {
+            _router.go('/home');
+          },
+          child: currentLoc == 'home' ? const Text(
+            'Home',
+            style: TextStyle(
+              color: Colors.white,
             ),
-          ),
-        ],
-      ),
+          ) : const Text('Home'),
+        ),
+        Container(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary,
+          height: 20,
+        ),
+        TextButton(
+          onPressed: () {
+            _router.go('/about');
+          },
+          child: currentLoc == 'about' ? const Text(
+              'About',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+          ) : const Text('About'),
+        ),
+        Container(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary,
+          height: 20,
+        ),
+        TextButton(
+          onPressed: () {
+            _router.go('/demos');
+          },
+          child: currentLoc == 'demos' ? const Text(
+              'Demos',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+          ) : const Text('Demos'),
+        ),
+      ],
     );
   }
 }
@@ -155,17 +330,82 @@ class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Page not found'),
+      body: Column(
+        children: [
+          Image.asset('signature.png'),
+          NavTabBar(),
+          const Flexible(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('🤷‍♂️', style: TextStyle(fontSize: 96)),
+                          Text('Page not found', style: TextStyle(fontSize: 24)),
+                          Text('404'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Title(
+      title: title,
+      color: Theme.of(context).colorScheme.primary,
+      child: Scaffold(
+        body: Column(
           children: [
-            // massive shrug emoji
-            Text('🤷‍♂️', style: TextStyle(fontSize: 96)),
-            Text('Page not found', style: TextStyle(fontSize: 24)),
-            Text('404'),
+            Image.asset('signature.png'),
+            NavTabBar(),
+            const Center(
+              child: Text('About Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DemoPage extends StatelessWidget {
+  const DemoPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Title(
+      title: title,
+      color: Theme.of(context).colorScheme.primary,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Image.asset('signature.png'),
+            NavTabBar(),
+            const Center(
+              child: Text('Demo Page'),
+            ),
           ],
         ),
       ),
